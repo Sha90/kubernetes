ed317b7a049df6cd543be5e09fa3b5986202558d
Sharath Kumar <skumar@xanadu.ie>
2020-07-13 22:24:21 +0100

    ubuntu cluster

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -10,68 +10,58 @@ EOF
 
 # Install docker from Docker-ce repository
 echo "[TASK 2] Install docker container engine"
-yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
-yum install -y -q docker-ce >/dev/null 2>&1
+apt-get update && \
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent \
+                   software-properties-common > /dev/null 2>&1
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
 
 # Enable docker service
-echo "[TASK 3] Enable and start docker service"
-systemctl enable docker >/dev/null 2>&1
-systemctl start docker
-
-# Disable SELinux
-echo "[TASK 4] Disable SELinux"
-setenforce 0
-sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-
-# Stop and disable firewalld
-echo "[TASK 5] Stop and Disable firewalld"
-systemctl disable firewalld >/dev/null 2>&1
-systemctl stop firewalld
+#echo "[TASK 3] Enable and start docker service"
+#systemctl enable docker >/dev/null 2>&1
+#systemctl start docker
 
 # Add sysctl settings
-echo "[TASK 6] Add sysctl settings"
-cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+echo "[TASK 4] Add sysctl settings"
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl --system >/dev/null 2>&1
 
 # Disable swap
-echo "[TASK 7] Disable and turn off SWAP"
+echo "[TASK 5] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-# Add yum repo file for Kubernetes
-echo "[TASK 8] Add yum repo file for kubernetes"
-cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# Add apt repo file for Kubernetes
+echo "[TASK 6] Add apt repo file for kubernetes"
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+apt-get update
 
 # Install Kubernetes
-echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
-yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+echo "[TASK 7] Install Kubernetes (kubeadm, kubelet and kubectl)"
+apt-get install -y kubelet kubeadm kubectl >/dev/null 2>&1
+apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 
 # Start and Enable kubelet service
-echo "[TASK 10] Enable and start kubelet service"
-systemctl enable kubelet >/dev/null 2>&1
-systemctl start kubelet >/dev/null 2>&1
+echo "[TASK 8] Enable and start kubelet service"
+systemctl daemon-reload >/dev/null 2>&1
+systemctl restart kubelet >/dev/null 2>&1
 
 # Enable ssh password authentication
-echo "[TASK 11] Enable ssh password authentication"
+echo "[TASK 9] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl reload sshd
 
 # Set Root password
-echo "[TASK 12] Set root password"
-echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
+#echo "[TASK 10] Set root password"
+#echo "kubeadmin" | passwd root >/dev/null 2>&1
 
 # Update vagrant user's bashrc file
 echo "export TERM=xterm" >> /etc/bashrc

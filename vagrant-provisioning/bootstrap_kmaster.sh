0f058f60625c4558ef944a4bda959a9cd78d00b5
Sharath Kumar <skumar@xanadu.ie>
2020-07-14 00:21:47 +0100

    added kubens and kubectx

--- vagrant-provisioning/bootstrap_kmaster.sh
+++ vagrant-provisioning/bootstrap_kmaster.sh
@@ -17,3 +17,35 @@ su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.11/manifest
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh
+
+# install bash-completion
+echo "Install bash completion"
+apt-get install -y bash-completion
+source /usr/share/bash-completion/bash_completion
+echo 'source <(kubectl completion bash)' >> /etc/bashrc
+kubectl completion bash >/etc/bash_completion.d/kubectl
+
+# install kubens and kubectx
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+#kubens auto completion
+_kube_namespaces()
+{
+  local curr_arg;
+  curr_arg=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg ) );
+}
+
+complete -F _kube_namespaces kubens kns
+
+#kubectx auto completion
+_kube_contexts()
+{
+  local curr_arg;
+  curr_arg=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
+}
+
+complete -F _kube_contexts kubectx kctx
